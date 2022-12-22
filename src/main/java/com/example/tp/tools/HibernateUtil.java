6c2698401aacd490df5fcc3ad83b0c4bd8d99483
tekcay <pierreblc@hotmail.fr>
2022-12-22 14:29:54 +0100

    added classes to handle sessions

---
+++ src/main/java/com/example/tp/tools/HibernateUtil.java
@@ -0,0 +1,20 @@
+package com.example.tp.tools;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sesssionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        try {
+            return new Configuration().configure("static/hibernate.cfg.xml").buildSessionFactory();
+        }catch (HibernateException e) {
+            throw e;
+        }
+    }
+
+    public static SessionFactory getSesssionFactory() {return buildSessionFactory();}
+}
